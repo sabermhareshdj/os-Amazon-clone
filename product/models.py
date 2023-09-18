cb6442bdfe4e2f2154b713617404df913cb246cd
Saber Mharesh <dj.saber.mharesh@gmail.com>
2023-09-18 15:03:45 +0300

    add str mothod

--- product/models.py
+++ product/models.py
@@ -20,13 +20,18 @@ class Product(models.Model):
     description = models.TextField(_('Description'),max_length=40000)
     quantity = models.IntegerField(_('Quantity'))
     brand = models.ForeignKey('Brand',verbose_name=_('Brand'),related_name='product_brand',on_delete=models.SET_NULL,null=True)
-
+ 
+    def __str__(self):
+        return self.name
     
 
 
 class ProductImages(models.Model):
     product = models.ForeignKey(Product,verbose_name=_('Product'),related_name='product_image',on_delete=models.CASCADE)
     image = models.ImageField(_('Image'),upload_to='product_images')
+
+    def __str__(self):
+        return str(self.product)
 
 class Brand(models.Model):
     name = models.CharField(_('Name'),max_length=100)
@@ -41,4 +46,7 @@ class Review(models.Model):
     rate = models.IntegerField(_('Rate'))
     review = models.CharField(_('Review'),max_length=300)
     created_at = models.DateTimeField(_('Created at'),default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user} - {self.product}"
 
